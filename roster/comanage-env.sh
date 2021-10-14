0a23563bd28e5f029ff247f40df10746413e1081
Duncan Brown <dabrown@syr.edu>
2021-10-14 10:44:53 -0400

    change ce to np3m

--- roster/comanage-env.sh
+++ roster/comanage-env.sh
@@ -1,5 +1,5 @@
 export STORAGE_PATH=/srv/docker/comanage
-export APACHE_SHIBD_DIR=/root/ce-roster/apache-shibd
+export APACHE_SHIBD_DIR=/root/np3m-roster/apache-shibd
 export COMANAGE_REGISTRY_VERSION=3.2.4
 export COMANAGE_REGISTRY_BASE_IMAGE_VERSION=1
 export COMANAGE_REGISTRY_SHIBBOLETH_SP_BASE_IMAGE_VERSION=1
@@ -8,11 +8,11 @@ export COMANAGE_REGISTRY_SHIBBOLETH_SP_VERSION=3.2.1
 export COMANAGE_REGISTRY_ADMIN_GIVEN_NAME=Duncan
 export COMANAGE_REGISTRY_ADMIN_FAMILY_NAME=Brown
 export COMANAGE_REGISTRY_ADMIN_USERNAME=dabrown@syr.edu
-export COMANAGE_REGISTRY_VIRTUAL_HOST_FQDN=roster.cosmicexplorer.org
+export COMANAGE_REGISTRY_VIRTUAL_HOST_FQDN=roster.np3m.org
 export COMANAGE_REGISTRY_SLAPD_BASE_IMAGE_VERSION=1
 export COMANAGE_REGISTRY_SLAPD_IMAGE_VERSION=1
-export OLC_SUFFIX=dc=cosmicexplorer,dc=org
-export OLC_ROOT_DN=cn=admin,dc=cosmicexplorer,dc=org
+export OLC_SUFFIX=dc=np3m,dc=org
+export OLC_ROOT_DN=cn=admin,dc=np3m,dc=org
 export COMANAGE_REGISTRY_ENABLE_PLUGIN=MailmanProvisioner,ChangelogProvisioner,GithubProvisioner,RestDccProvisioner
 export MARIADB_ROOT_PASSWD=badgers
 export MARIADB_PASSWD=badgers
